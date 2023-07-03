145bde188a24d9c5c847c88f2b5ca2c1534b5c04
yudai1204 <yudai1204h@gmail.com>
2023-07-03 10:26:57 +0900

    add `output: 'export'`

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@
 const branchName = process.env.BRANCH_NAME ? "/" + process.env.BRANCH_NAME : "";
 
 const nextConfig = {
+    output: 'export',
     reactStrictMode: true,
     assetPrefix: branchName,
     basePath: branchName,
